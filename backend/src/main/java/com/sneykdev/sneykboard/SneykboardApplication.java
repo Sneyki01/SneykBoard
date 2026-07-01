8ddd2103f96a8d27b305115137ceac2f3727641f
SneykiLap <idar.herrera@gmail.com>
2026-07-01 14:25:31 -0600

    chore: initialize Spring Boot backend

---
+++ backend/src/main/java/com/sneykdev/sneykboard/SneykboardApplication.java
@@ -0,0 +1,13 @@
+package com.sneykdev.sneykboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SneykboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SneykboardApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/sneykdev/sneykboard/SneykboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sneykdev.sneykboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SneykboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
